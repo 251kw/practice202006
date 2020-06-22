c2feda7ff8b3580dade1c6627681e350ef27bfb2
d.ito <d.ito@NP-21-098-0.shantery.local>
2020-06-22 11:45:44 +0900

    初期コミット

---
+++ src/dao/SnsDAO.java
@@ -0,0 +1,64 @@
+package dao;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.jdbc.Connection;
+
+public class SnsDAO {
+	private final String DSN = "jdbc:mysql://localhost:3306/sns2?useSSL=false";
+	private final String USER = "root";
+	private final String PASSWORD = "root";
+
+	public Connection getConnection() {
+		Connection conn = null;
+
+		try {
+			//JDBC[
+			Class.forName("com.mysql.jdbc.Driver");
+
+			//データベース接続
+			conn = (Connection) DriverManager.getConnection(DSN,USER,PASSWORD);
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return conn;
+	}
+
+	//connection 型変数が持つデータベース
+	public void close(Connection conn) {
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	//PreparedStatment JDBC リソースの開放
+	public void close(Statement stmt) {
+		if(stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	//REsult JDBC リソースの開放
+	public void close(ResultSet rset) {
+		if(rset != null) {
+			try {
+				rset.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
